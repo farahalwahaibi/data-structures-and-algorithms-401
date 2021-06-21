c62471dd6f23f4f983661f6d9c74284ebefeb149
farahalwahaibi <farah.alwahaibi77@gmail.com>
2021-06-21 22:40:52 +0300

    mergeSort

---
+++ code-challenge27/__test__/Merge-Sort.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// require jest
+const { expect, describe, it } = require( '@jest/globals' );
+// require Insertion-Sort
+const mergeSort = require( '../Merge-Sort.js' );
+
+
+//start testing
+describe( 'insertionSort', () => {
+
+  it( 'should merge the array after sort it', () => {
+    //arrange
+    let arr =[8,4,23,42,16,15];
+    //act
+    let sortedMergedArr= mergeSort( arr );
+    //assert
+    expect( sortedMergedArr ).toEqual( [ 4, 8, 15, 16, 23, 42 ] );
+  } );
+
+  it( 'should merge the array after sort it', () => {
+    //arrange
+    let num =555;
+    //act
+    let sortedMergedArr= mergeSort( num );
+    //assert
+    expect( sortedMergedArr ).toBe( num );
+  } );
+
+} );
